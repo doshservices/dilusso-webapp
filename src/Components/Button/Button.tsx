6cc4e1a961a67088ce43b109e41389361aa9d98e
Jozz77 <mmadujonathan@gmail.com>
2024-05-14 19:30:10 +0100

    new

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -9,7 +9,7 @@ export default function Button(props: Props) {
   return (
     <Link
       to={props.link}
-      className=" text-[#222222] text-[0.9rem] sm:text-[0.8rem] lg:text-[0.85rem] xxl:text-[0.9rem] border-[1px] font-semibold hover:bg-black hover:text-White border-Black py-2 sm:py-2 lg:py-2 px-5 sm:px-4 lg:px-6  "
+      className=" text-[#222222] text-[0.9rem] sm:text-[0.8rem] lg:text-[0.85rem] xxl:text-[0.9rem] border-[1px] font-semibold hover:bg-black hover:text-White border-Black py-3 sm:py-2 lg:py-2 px-5 sm:px-4 lg:px-6  "
     >
       {props.text}
     </Link>
