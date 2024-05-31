a279a517e5be22b57789e660b14a2d5f864961fc
prosper edafe <mamusedafe19@gmail.com>
2024-05-31 19:22:23 +0100

    "Added hero image and grid of 12 product cards with images, designer names, prices, and descriptions to Category page"

--- src/Pages/Category.tsx
+++ src/Pages/Category.tsx
@@ -1,6 +1,109 @@
+import womenHeroimg from '../assets/Frame 1000007698.png'
+import shirt from '../assets/t-shirt.png';
+
 const Category = () => {
     return (
-        <></>
+        <div className='pt-20 pb-8 sm:pt-36 px-[5%]'>
+            <img src={womenHeroimg} alt="" />
+            <div className='grid grid-cols-2 sm:grid-cols-3 gap-4'>
+                <div className='p-3'>
+                    <img src={shirt} className='w-full mb-2' alt="" />
+                    <div className='flex items-center justify-between'>
+                        <p className='text-sm font-medium text-[#333333]'>Designer Name</p>
+                        <p className='text-sm font-medium text-[#333333]'>$400</p>
+                    </div>
+                    <p className='text-[#000000e6] text-sm mt-3'>Little description</p>
+                </div>
+                <div className='p-3'>
+                    <img src={shirt} className='w-full mb-2' alt="" />
+                    <div className='flex items-center justify-between'>
+                        <p className='text-sm font-medium text-[#333333]'>Designer Name</p>
+                        <p className='text-sm font-medium text-[#333333]'>$400</p>
+                    </div>
+                    <p className='text-[#000000e6] text-sm mt-3'>Little description</p>
+                </div>
+                <div className='p-3'>
+                    <img src={shirt} className='w-full mb-2' alt="" />
+                    <div className='flex items-center justify-between'>
+                        <p className='text-sm font-medium text-[#333333]'>Designer Name</p>
+                        <p className='text-sm font-medium text-[#333333]'>$400</p>
+                    </div>
+                    <p className='text-[#000000e6] text-sm mt-3'>Little description</p>
+                </div>
+                <div className='p-3'>
+                    <img src={shirt} className='w-full mb-2' alt="" />
+                    <div className='flex items-center justify-between'>
+                        <p className='text-sm font-medium text-[#333333]'>Designer Name</p>
+                        <p className='text-sm font-medium text-[#333333]'>$400</p>
+                    </div>
+                    <p className='text-[#000000e6] text-sm mt-3'>Little description</p>
+                </div>
+                <div className='p-3'>
+                    <img src={shirt} className='w-full mb-2' alt="" />
+                    <div className='flex items-center justify-between'>
+                        <p className='text-sm font-medium text-[#333333]'>Designer Name</p>
+                        <p className='text-sm font-medium text-[#333333]'>$400</p>
+                    </div>
+                    <p className='text-[#000000e6] text-sm mt-3'>Little description</p>
+                </div>
+                <div className='p-3'>
+                    <img src={shirt} className='w-full mb-2' alt="" />
+                    <div className='flex items-center justify-between'>
+                        <p className='text-sm font-medium text-[#333333]'>Designer Name</p>
+                        <p className='text-sm font-medium text-[#333333]'>$400</p>
+                    </div>
+                    <p className='text-[#000000e6] text-sm mt-3'>Little description</p>
+                </div>
+                <div className='p-3'>
+                    <img src={shirt} className='w-full mb-2' alt="" />
+                    <div className='flex items-center justify-between'>
+                        <p className='text-sm font-medium text-[#333333]'>Designer Name</p>
+                        <p className='text-sm font-medium text-[#333333]'>$400</p>
+                    </div>
+                    <p className='text-[#000000e6] text-sm mt-3'>Little description</p>
+                </div>
+                <div className='p-3'>
+                    <img src={shirt} className='w-full mb-2' alt="" />
+                    <div className='flex items-center justify-between'>
+                        <p className='text-sm font-medium text-[#333333]'>Designer Name</p>
+                        <p className='text-sm font-medium text-[#333333]'>$400</p>
+                    </div>
+                    <p className='text-[#000000e6] text-sm mt-3'>Little description</p>
+                </div>
+                <div className='p-3'>
+                    <img src={shirt} className='w-full mb-2' alt="" />
+                    <div className='flex items-center justify-between'>
+                        <p className='text-sm font-medium text-[#333333]'>Designer Name</p>
+                        <p className='text-sm font-medium text-[#333333]'>$400</p>
+                    </div>
+                    <p className='text-[#000000e6] text-sm mt-3'>Little description</p>
+                </div>
+                <div className='p-3'>
+                    <img src={shirt} className='w-full mb-2' alt="" />
+                    <div className='flex items-center justify-between'>
+                        <p className='text-sm font-medium text-[#333333]'>Designer Name</p>
+                        <p className='text-sm font-medium text-[#333333]'>$400</p>
+                    </div>
+                    <p className='text-[#000000e6] text-sm mt-3'>Little description</p>
+                </div>
+                <div className='p-3'>
+                    <img src={shirt} className='w-full mb-2' alt="" />
+                    <div className='flex items-center justify-between'>
+                        <p className='text-sm font-medium text-[#333333]'>Designer Name</p>
+                        <p className='text-sm font-medium text-[#333333]'>$400</p>
+                    </div>
+                    <p className='text-[#000000e6] text-sm mt-3'>Little description</p>
+                </div>
+                <div className='p-3'>
+                    <img src={shirt} className='w-full mb-2' alt="" />
+                    <div className='flex items-center justify-between'>
+                        <p className='text-sm font-medium text-[#333333]'>Designer Name</p>
+                        <p className='text-sm font-medium text-[#333333]'>$400</p>
+                    </div>
+                    <p className='text-[#000000e6] text-sm mt-3'>Little description</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
